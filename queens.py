154ec0cb69194e695593613a352e51a931addd1e
cdoig4 <cdoig4@my.bcit.ca>
2022-11-23 20:21:49 -0800

    Create class and queen NPC dictionaries

--- queens.py
+++ queens.py
@@ -2,8 +2,22 @@
 Populate the  Charisma, Uniqueness, Nerve & Talent for each queen type and enemy queen.
 """
 
-look_queen = {()}
-comedy_queen = {()}
+look_queen = {"Charisma": 2, "Uniqueness": 4, "Nerve": 7, "Talent": 5}
+comedy_queen = {"Charisma": 5, "Uniqueness": 4, "Nerve": 2, "Talent": 7}
+performance_queen = {"Charisma": 7, "Uniqueness": 2, "Nerve": 5, "Talent": 4}
 
 
-queen_name = {()}
+
+
+queen_aja = {"Name": "Aja", "Charisma": 25, "Uniqueness": 30, "Nerve": 35, "Talent": 35}
+queen_jinkx_monsoon = {"Name": "Jinkx", "Charisma": 60, "Uniqueness": 60, "Nerve": 55, "Talent": 70}
+queen_shea_coulee = {"Name": "Shea", "Charisma": 63, "Uniqueness": 61, "Nerve": 70, "Talent": 70}
+queen_serena_cha_cha = {"Name": "Serena ChaCha", "Charisma": 13, "Uniqueness": 15, "Nerve": 21, "Talent": 12}
+queen_derrick_berry = {"Name": "Derrick", "Charisma": 21, "Uniqueness": 14, "Nerve": 17, "Talent": 18}
+queen_bob_the_drag_queen = {"Name": "Bob", "Charisma": 65, "Uniqueness": 64, "Nerve": 64, "Talent": 67}
+queen_katya_zamalodchikova = {"Name": "Katya", "Charisma": 43, "Uniqueness": 46, "Nerve": 33, "Talent": 37}
+queen_kennedy_davenport = {"Name": "Kennedy", "Charisma": 34, "Uniqueness": 32, "Nerve": 36, "Talent": 30}
+queen_shangela_laquifa_wadley = {"Name": "Shangela", "Charisma": 57, "Uniqueness": 45, "Nerve": 39, "Talent": 56}
+queen_lashauwn_beyond = {"Name": "LaShauwn", "Charisma": 12, "Uniqueness": 16, "Nerve": 12, "Talent": 14}
+
+queen_bitch_rupaul = {"Name": "RuPaul", "Charisma": 80, "Uniqueness": 75, "Nerve": 80, "Talent": 80}
