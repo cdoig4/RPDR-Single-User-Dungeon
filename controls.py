eede9356709b5443f860102e0c31ccad975ca0fa
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-27 15:05:43 -0800

    Correct for loop inclusion syntax

--- controls.py
+++ controls.py
@@ -16,7 +16,7 @@ def generate_challenge_input(answers: list):
     """
     pairs = []
 
-    for number, answer in enumerate(answers, 1):
+    for number, answer in enumerate([answers], 1):
         pair = (number, answer)
         pairs.append(pair)
     return pairs
@@ -26,6 +26,7 @@ def main():
     Drive the program
     """
     print(generate_directional_tools())
+    print(generate_challenge_input("answer 1, answer 2, answer 3, answer 4"))
 
 if __name__ == '__main__':
     main()
