33ae03d3a7ef5e288a5fecce2261c90d6f10e95f
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-27 16:35:16 -0800

    Complete get_input_from_user logic

--- controls.py
+++ controls.py
@@ -21,16 +21,34 @@ def generate_challenge_input(answers: list) -> list:
         pairs.append(pair)
     return pairs
 
-# def format_output():
+def get_input_from_user(game_input: list) -> str:
+    """
 
+    :param game_input:
+    :return:
+    """
+    acceptable_answers = []
 
+    for pair in game_input:
+        acceptable_answers += pair[0]
+        print(f'{pair[0]}: {pair[1]}')
+
+    answer = input()
+    if answer not in acceptable_answers:
+        print("That is not an acceptable answer! Please try again:")
+        return get_input_from_user(game_input)
+
+    answer_index = acceptable_answers.index(answer)
+
+    return game_input[answer_index][1]
 
 def main():
     """
     Drive the program
     """
-    print(generate_directional_tools())
-    print(generate_challenge_input(['answer 1', 'answer 2', 'answer 3', 'answer 4']))
+    # print(generate_directional_tools())
+    print(get_input_from_user(generate_directional_tools()))
+    # print(generate_challenge_input(['answer 1', 'answer 2', 'answer 3', 'answer 4']))
 
 if __name__ == '__main__':
     main()
