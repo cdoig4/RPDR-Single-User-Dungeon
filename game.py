bae9659c5ad17d2b1e791336ff6ea51dc46e6f63
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-29 19:59:36 -0800

    Add display_board and move_character to game()

--- game.py
+++ game.py
@@ -5,23 +5,27 @@ Kelly Hagg A01324804
 import character_setup_and_intro
 
 import boards
+import controls
 
-
-def make_board(character):
+def set_board(character):
     """
 
     :param character:
     :return:
     """
     if character.get('met_rupaul'):
-        board = boards.read_board('dressing_room')
+        character.update({'location': 'dressing_room'})
+        character.update({'coordinates': (1, 5)})
     elif character.get('completed_lip_sync'):
-        board = boards.read_board('judges_panel')
+        character.update({'location': 'judges_panel'})
+        character.update({'coordinates': (1, 6)})
     elif character.get('level') == 2:
-        board = boards.read_board('stage')
+        character.update({'location': 'stage'})
+        character.update({'coordinates': (0, 5)})
     else:
-        board = boards.read_board('werkroom')
-    return board
+        character.update({'location': 'werkroom'})
+        character.update({'coordinates': (0, 4)})
+    return character
 
 
 def get_user_choice():
@@ -74,22 +78,26 @@ def game():
     character = character_setup_and_intro.make_character(input("What is the name of your Drag Persona?\n"))
     character_setup_and_intro.deliver_introduction(character)
     achieved_goal = False
+
     while not achieved_goal:
-        board = make_board(character)
-        describe_current_location(board, character)
-        direction = get_user_choice()
-        valid_move = validate_move(board, character, direction)
-        if valid_move:
-            move_character(character)
-        describe_current_location(board, character)
-        there_is_a_challenge = check_for_challenges()
-        if there_is_a_challenge:
-            execute_challenge_protocol(character)
-        if character_has_leveled():
-            execute_glow_up_protocol()
-            achieved_goal = check_if_goal_attained(board, character)
-        else:
-            print('You have to achieve your goal!')
+        if character.get('location') == None:
+            set_board(character)
+        boards.display_board(character)
+        controls.move_character(character)
+        # describe_current_location(board, character)
+        # direction = get_user_choice()
+        # valid_move = validate_move(board, character, direction)
+        # if valid_move:
+        #     move_character(character)
+        # describe_current_location(board, character)
+        # there_is_a_challenge = check_for_challenges()
+        # if there_is_a_challenge:
+        #     execute_challenge_protocol(character)
+        # if character_has_leveled():
+        #     execute_glow_up_protocol()
+        #     achieved_goal = check_if_goal_attained(board, character)
+        # else:
+        #     print('You have to achieve your goal!')
 
 
 def main():
