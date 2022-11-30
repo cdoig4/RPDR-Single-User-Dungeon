dd1803f4fb6dd21fc4a382be0fd7ef0919decd15
cdoig4 <cdoig4@my.bcit.ca>
2022-11-30 10:15:17 -0800

    Reformat queen variables

--- queens.py
+++ queens.py
@@ -1,14 +1,18 @@
 """
 Populate the  Charisma, Uniqueness, Nerve & Talent for each queen type and enemy queen.
 """
-queen_aja_labeija = {"Name": "Aja", "Charisma": 25, "Uniqueness": 30, "Nerve": 35, "Talent": 35}
-queen_jinkx_monsoon = {"Name": "Jinkx", "Charisma": 60, "Uniqueness": 60, "Nerve": 55, "Talent": 70}
-queen_shea_coulee = {"Name": "Shea", "Charisma": 63, "Uniqueness": 61, "Nerve": 70, "Talent": 70}
-queen_serena_cha_cha = {"Name": "Serena ChaCha", "Charisma": 13, "Uniqueness": 15, "Nerve": 21, "Talent": 12}
-queen_derrick_berry = {"Name": "Derrick", "Charisma": 21, "Uniqueness": 14, "Nerve": 17, "Talent": 18}
-queen_bob_the_drag_queen = {"Name": "Bob", "Charisma": 65, "Uniqueness": 64, "Nerve": 64, "Talent": 67}
-queen_katya_zamalodchikova = {"Name": "Katya", "Charisma": 43, "Uniqueness": 46, "Nerve": 33, "Talent": 37}
-queen_kennedy_davenport = {"Name": "Kennedy", "Charisma": 34, "Uniqueness": 32, "Nerve": 36, "Talent": 30}
-queen_shangela_laquifa_wadley = {"Name": "Shangela", "Charisma": 57, "Uniqueness": 45, "Nerve": 39, "Talent": 56}
-queen_lashauwn_beyond = {"Name": "LaShauwn", "Charisma": 12, "Uniqueness": 16, "Nerve": 12, "Talent": 14}
-queen_bitch_rupaul = {"Name": "RuPaul", "Charisma": 80, "Uniqueness": 75, "Nerve": 80, "Talent": 80}
+queen_aja_labeija = {"Name": "Aja", "Charisma": 14, "Uniqueness": 17, "Nerve": 10}
+queen_jinkx_monsoon = {"Name": "Jinkx", "Charisma": 60, "Uniqueness": 60, "Nerve": 55}
+queen_shea_coulee = {"Name": "Shea", "Charisma": 13, "Uniqueness": 13, "Nerve": 16}
+queen_serena_cha_cha = {"Name": "Serena ChaCha", "Charisma": 13, "Uniqueness": 15, "Nerve": 5}
+queen_derrick_berry = {"Name": "Derrick", "Charisma": 11, "Uniqueness": 14, "Nerve": 9}
+queen_bob_the_drag_queen = {"Name": "Bob", "Charisma": 16, "Uniqueness": 12, "Nerve": 14}
+queen_katya_zamalodchikova = {"Name": "Katya", "Charisma": 13, "Uniqueness": 17, "Nerve": 13}
+queen_kennedy_davenport = {"Name": "Kennedy", "Charisma": 15, "Uniqueness": 15, "Nerve": 15}
+queen_shangela_laquifa_wadley = {"Name": "Shangela", "Charisma": 18, "Uniqueness": 18, "Nerve": 15}
+queen_lashauwn_beyond = {"Name": "LaShauwn", "Charisma": 12, "Uniqueness": 16, "Nerve": 7}
+queen_bitch_rupaul = {"Name": "RuPaul", "Charisma": 80, "Uniqueness": 75, "Nerve": 80}
+
+potential_queen_challengers = [queen_aja_labeija, queen_jinkx_monsoon, queen_shea_coulee, queen_serena_cha_cha,
+                               queen_derrick_berry, queen_bob_the_drag_queen, queen_katya_zamalodchikova,
+                               queen_kennedy_davenport, queen_shangela_laquifa_wadley, queen_lashauwn_beyond]
