aa5c91f41afadf1d4b414112c900744a96cd0d7f
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-30 20:26:57 -0800

    Remove unneeded newline characters

--- boards.py
+++ boards.py
@@ -160,7 +160,7 @@ def display_board(character: dict) -> None:
 
     descriptions = location_descriptions.get(board_name)
 
-    print(descriptions[0])
+    print(f'\n{descriptions[0]}')
 
     if board_indices.get(current_coordinates) == 'exit':
         print(descriptions[1])
