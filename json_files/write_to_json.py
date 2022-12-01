9f21987606e48a5f3cda6e76b989a412ee448002
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-30 16:02:41 -0800

    Write potential_queen_challengers to json

--- json_files/write_to_json.py
+++ json_files/write_to_json.py
@@ -7,6 +7,72 @@ Kelly Hagg A01324804
 import json
 
 
+""" Queen Challengers """
+# potential_queen_challengers = [{'queen_aja_labeija': {"Name": "Aja", "Charisma": 14, "Uniqueness": 17, "Nerve": 10}},
+#                                {'queen_jinkx_monsoon': {"Name": "Jinkx", "Charisma": 60, "Uniqueness": 60, "Nerve": 55}},
+#                                {'queen_shea_coulee': {"Name": "Shea", "Charisma": 13, "Uniqueness": 13, "Nerve": 16}},
+#                                {'queen_serena_cha_cha': {"Name": "Serena ChaCha", "Charisma": 13, "Uniqueness": 15, "Nerve": 5}},
+#                                 {'queen_derrick_berry': {"Name": "Derrick", "Charisma": 11, "Uniqueness": 14, "Nerve": 9}},
+#                                {'queen_bob_the_drag_queen': {"Name": "Bob", "Charisma": 16, "Uniqueness": 12, "Nerve": 14}},
+#                                {'queen_katya_zamalodchikova': {"Name": "Katya", "Charisma": 13, "Uniqueness": 17, "Nerve": 13}},
+#                                {'queen_kennedy_davenport': {"Name": "Kennedy", "Charisma": 15, "Uniqueness": 15, "Nerve": 15}},
+#                                {'queen_shangela_laquifa_wadley': {"Name": "Shangela", "Charisma": 18, "Uniqueness": 18, "Nerve": 15}},
+#                                {'queen_lashauwn_beyond': {"Name": "LaShauwn", "Charisma": 12, "Uniqueness": 16, "Nerve": 7}},
+#                                {'queen_bitch_rupaul': {"Name": "RuPaul", "Charisma": 80, "Uniqueness": 75, "Nerve": 80}}]
+#
+# filename = 'potential_queen_challengers.json'
+# with open(filename, 'w') as file_object:
+#     json.dump(potential_queen_challengers, file_object)
+
+""" Lip Sync Challenge Data"""
+# lip_sync_data = {'CHER_LIP_SYNC_CHALLENGE': {'Song Title': 'Believe by Cher',
+#                            'Correct Answer': ['No matter how hard I try, you keep pushing me aside',
+#                                               'Do you believe in life after love?',
+#                                               "Well I know that I'll get through this, Cause I know that I am strong"],
+#                            'Initial Lyrics': ['No matter how hard I try, you keep pushing me aside',
+#                                               'No matter how much effort I put in, you never let me win',
+#                                               'Regardless of what I do, I can never get to you'],
+#                            'Chorus Lyrics': ['Do you believe in sun after rain?',
+#                                              'Do you believe that you know the words?',
+#                                              'Do you believe in life after love?'],
+#                            'Final Lyrics': ["I know that I'll get through this, because I found a really cool frog",
+#                                             "Well I know that I'll get through this, Cause I know that I am strong",
+#                                             "Well I know that I'll get through this, Cause I know that I'm not wrong"]},
+#                                'MADONNA_LIP_SYNC_CHALLENGE': {'Song Title': 'Vogue by Madonna',
+#                               'Correct Answer':
+#                                   ['When all else fails and you long to be Something better than you are today',
+#                                    'Come on, vogue. Let your body move to the music',
+#                                    "Magical, life's a ball So get up on the dance floor"],
+#                               'Initial Lyrics':
+#                                   ['When all else fails and you long to be Somewhere other than you are right now',
+#                                    'When all else fails and you take a stand To make tomorrow a brighter day',
+#                                    'When all else fails and you long to be Something better than you are today'],
+#                               'Chorus Lyrics': ['Come on, vogue. Let your body move to the music',
+#                                                 'Come on, vogue. Let yourself get into the groovin',
+#                                                 "Hey let's vogue. Nod your head to the beat"],
+#                               'Final Lyrics': ["Beautiful, life's magical So let the music take you home",
+#                                                "Magic, in the ballroom So get onto the cat walk",
+#                                                "Magical, life's a ball So get up on the dance floor", ]},
+#                                'CARLY_RAE_JEPSEN_LIP_SYNC_CHALLENGE':
+#                                    {'Song Title': 'Run Away With Me by Carly Rae Jepsen', 'Correct Answer':
+#                                            ["You're stuck in my head, stuck in my heart, stuck in my body- body",
+#                                             'When the lights go out, Run away with me! Run away with me!',
+#                                             'Over the weekend, we could turn the world to gold'],
+#                                        'Initial Lyrics':
+#                                            ["You're stuck on my shoe, stuck on my sole, please get off me- off me",
+#                                             "You're stuck in my head, stuck in my heart, stuck in my body- body",
+#                                             "You're stuck in my head, stuck in my heart, we're both at the party-party"],
+#                                        'Chorus Lyrics': ['When the power goes out, Come and look for me! Come and look for me!',
+#                                                          'When the lights go out, Run away with me! Run away with me!',
+#                                                          'When the lights go out, We have to leave! We have to leave!'],
+#                                        'Final Lyrics': ["What's that smell, I think it might be mold.",
+#                                                         'Over the weekend, we could turn the world to gold',
+#                                                         'Over the summer, we could make the world turn gold']}}
+# filename = 'lip_sync_data.json'
+# with open(filename, 'w') as file_object:
+#     json.dump(lip_sync_data, file_object)
+
+""" Location Descriptions """
 # location_descriptions = {'werk_room': ('You are currently in the Drag Race Werk Room. '
 #                                        'There are mirrors along the side\nof the room, a '
 #                                        'fabric wall covered in rolls of fabric, and several '
