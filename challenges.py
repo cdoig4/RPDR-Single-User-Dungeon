6f8270f92b53d334256929425a889d7acdbd63f4
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-11-30 18:26:08 -0800

    Add perform_lip_sync param to runway in run_challenges

--- challenges.py
+++ challenges.py
@@ -217,7 +217,7 @@ def run_challenges(character):
     if location == 'werk_room' and coordinates != (0, 4) and coordinates != (6, 4):
         return werk_room_events(character)
     if location == 'judges_panel' and coordinates != (1, 6) and coordinates != (2, 0):
-        return runway_event(character)
+        return runway_event(perform_lip_sync, character)
 
 
 def main():
