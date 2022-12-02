1c14c35794cbf5331734fcb7825d5dff82d6f760
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-01 22:08:20 -0800

    Remove rupaul from possible queens in fight

--- challenges.py
+++ challenges.py
@@ -140,7 +140,10 @@ def fight(character):
     filename = './json_files/queens.json'
     with open(filename) as file_object:
         queens = json.load(file_object)
+
     queen_names = list(queens.keys())
+    queen_names.remove('queen_bitch_rupaul')
+    print(queen_names)
     enemy_queen = random.choice(queen_names)
 
     print(f"{queens[enemy_queen]['Name']} approaches you, placing the dreaded Reading Glasses on her face as the "
@@ -210,41 +213,42 @@ def final_lip_sync(character):
     return character.update({'Nerve': 0})
 
 
-def final_battle(character_dictionary):
+def final_battle(character):
     """
 
-    :param character_dictionary:
+    :param character:
     """
     filename = './json_files/queens.json'
     with open(filename) as file_object:
         queens = json.load(file_object)
         queen_bitch_rupaul = queens.get('queen_bitch_rupaul')
 
-    while queen_bitch_rupaul['Nerve'] > 35 and character_dictionary['Nerve'] > 0:
+    while queen_bitch_rupaul['Nerve'] > 35 and character['Nerve'] > 0:
         print("Mother stands strong, what will you do?")
         player_choice = get_challenge_input_from_user(['Read', 'Act Unimpressed', 'Flee'])
         if player_choice == 'Read':
             if random.randint(1, 20) > 2:
                 print(f"You read {queen_bitch_rupaul['Name']} for the gods... her eye twitches slightly.")
-                queen_bitch_rupaul['Nerve'] -= (random.randint(1, 8) + math.ceil(character_dictionary['Charisma'] / 4))
+                queen_bitch_rupaul['Nerve'] -= (random.randint(1, 8) + math.ceil(character['Charisma'] / 4))
             else:
                 print(f"Your read falls flat and {queen_bitch_rupaul['Name']} chuckles.")
         elif player_choice == 'Act Unimpressed':
             print("You are emotionally preparing yourself for RuPaul to read you for filth.")
-            character_dictionary['Uniqueness'] += 2
+            character['Uniqueness'] += 2
         else:
             print(f'{queen_bitch_rupaul["Name"]} says: "That\'s cute. You are staying right here till we\'re done"')
 
         if random.randint(1, 20) > 4:
             damage_to_player = random.randint(1, 7) + math.ceil(queen_bitch_rupaul['Charisma'] / 10)
-            character_dictionary['Nerve'] -= damage_to_player
+            character['Nerve'] -= damage_to_player
             print(f"{queen_bitch_rupaul['Name']} says {random.choice(RUPAUL_READS)}.\n"
                   f"Your Nerve is reduced by {damage_to_player}.")
         else:
             print(f"{queen_bitch_rupaul['Name']} throws out a read that goes over your head.")
 
-    if character_dictionary['Nerve'] > 0:
-        final_lip_sync(character_dictionary)
+    if character['Nerve'] > 0:
+        final_lip_sync(character)
+
 
 def generate_challenge_input(possible_answers: list) -> list:
     """
@@ -290,7 +294,8 @@ def run_challenges(character):
     location = character['location']
     coordinates = character['coordinates']
 
-    if location == 'werk_room' and coordinates != (0, 4) and coordinates != (6, 4) and character['level'] != 2:
+    if location == 'werk_room' and coordinates != (0, 4) and coordinates != (6, 4) \
+            and character['level'] != 2:
         if random.randint(1, 10) <= 3:
             return fight(character)
     if location == 'main_stage' and coordinates == (6, 8):
