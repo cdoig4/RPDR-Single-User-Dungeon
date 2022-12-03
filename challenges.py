8e8efc0784d3510eb0e720b5f98185f0aaedd49a
cdoig4 <cdoig4@my.bcit.ca>
2022-12-02 22:35:25 -0800

    Add docstring to a function in challenges.py
    Add docstring to generate_random_makeover_answers()

--- challenges.py
+++ challenges.py
@@ -31,9 +31,12 @@ POTENTIAL_READS = ('Legendary you think you are. Legendary? Looks like leg AND d
 
 def generate_random_makeover_answers(correct_answer):
     """
+    Generate randomized possible answers to makeover questions.
 
-    :param correct_answer:
-    :return:
+    :param correct_answer: a list representing the correct answer to the makeover question
+    :precondition: correct_answer must be a list
+    :postcondition: generate a tuple of lists representing possible answers to makeover question
+    :return: a tuple containing four lists of three elements each, one of which is the correct answer
     """
     all_permutations = list(itertools.permutations(correct_answer))
     all_permutations.remove(tuple(correct_answer))
