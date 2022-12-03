de3e595aafef6c1d960f7e55022cf61220ca023d
cdoig4 <cdoig4@my.bcit.ca>
2022-12-03 13:17:17 -0800

    Implement suite of unit tests for apply_power_up

---
+++ test_apply_power_up.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from character_setup import apply_power_up
+
+
+class TestApplyPowerUp(TestCase):
+    def test_empty_tuple(self):
+        self.assertEqual(None, apply_power_up((), 0))
+
+    def test_tuple_single_element(self):
+        self.assertEqual(None, apply_power_up(('test_string',), 0))
+
+    def test_tuple_first_element_empty_string_second_element_plus_value_less_than_zero(self):
+        self.assertEqual({'': 0}, apply_power_up(('', 110), -145))
+
+    def test_tuple_first_element_empty_string_second_element_plus_value_equals_zero(self):
+        self.assertEqual({'': 0}, apply_power_up(('', -45), 45))
+
+    def test_tuple_first_element_empty_string_second_element_plus_value_greater_than_zero(self):
+        self.assertEqual({'': 200}, apply_power_up(('', 135), 65))
+
+    def test_tuple_first_element_non_empty_string_second_element_plus_value_less_than_zero(self):
+        self.assertEqual({'test_string': 0}, apply_power_up(('test_string', 4), -10))
+
+    def test_tuple_first_element_non_empty_string_second_element_plus_value_equals_zero(self):
+        self.assertEqual({'test_string': 0}, apply_power_up(('test_string', 4), -4))
+
+    def test_tuple_first_element_non_empty_string_second_element_plus_value_greater_than_zero(self):
+        self.assertEqual({'test_string': 16}, apply_power_up(('test_string', 8), 8))
+
+
