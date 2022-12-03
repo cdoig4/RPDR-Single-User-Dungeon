ab8fc5884f39e699f3c3f7446e0da5f0223ed63b
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-03 15:47:57 -0800

    Complete test_make_character suite

--- test_make_character.py
+++ test_make_character.py
@@ -11,12 +11,59 @@ from character_setup import make_character
 
 class TestMakeCharacter(TestCase):
     @patch('challenges.get_challenge_input_from_user', return_value='look_queen')
-    def test_look_queen(self, get_challenge_input_from_user):
+    def test_make_character_look_queen(self, get_challenge_input_from_user):
         expected = {'Charisma': 14, 'Uniqueness': 15, 'Nerve': 10, 'Talent': 10,
                     'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
                     'Name': 'Ginger Snaps'}
         actual = make_character('Ginger Snaps')
         self.assertEqual(expected, actual)
 
+    @patch('challenges.get_challenge_input_from_user', return_value='comedy_queen')
+    def test_make_character_comedy_queen(self, get_challenge_input_from_user):
+        expected = {'Charisma': 15, 'Uniqueness': 14, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': 'Ginger Snaps'}
+        actual = make_character('Ginger Snaps')
+        self.assertEqual(expected, actual)
+
+    @patch('challenges.get_challenge_input_from_user', return_value='performance_queen')
+    def test_make_character_performance_queen(self, get_challenge_input_from_user):
+        expected = {'Charisma': 17, 'Uniqueness': 12, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': 'Ginger Snaps'}
+        actual = make_character('Ginger Snaps')
+        self.assertEqual(expected, actual)
+
+    @patch('challenges.get_challenge_input_from_user', return_value='alternative_queen')
+    def test_make_character_alternative_queen(self, get_challenge_input_from_user):
+        expected = {'Charisma': 12, 'Uniqueness': 17, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': 'Ginger Snaps'}
+        actual = make_character('Ginger Snaps')
+        self.assertEqual(expected, actual)
+
+    @patch('challenges.get_challenge_input_from_user', return_value='look_queen')
+    def test_make_character_lowercase_name(self, get_challenge_input_from_user):
+        expected = {'Charisma': 14, 'Uniqueness': 15, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': 'ginger snaps'}
+        actual = make_character('ginger snaps')
+        self.assertEqual(expected, actual)
+
+    @patch('challenges.get_challenge_input_from_user', return_value='look_queen')
+    def test_make_character_uppercase_name(self, get_challenge_input_from_user):
+        expected = {'Charisma': 14, 'Uniqueness': 15, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': 'GINGER SNAPS'}
+        actual = make_character('GINGER SNAPS')
+        self.assertEqual(expected, actual)
+
+    @patch('challenges.get_challenge_input_from_user', return_value='look_queen')
+    def test_make_character_numbers_and_punctuation_in_name(self, get_challenge_input_from_user):
+        expected = {'Charisma': 14, 'Uniqueness': 15, 'Nerve': 10, 'Talent': 10,
+                    'met_rupaul': False, 'completed_lip_sync': False, 'level': 1,
+                    'Name': '$@$G1Ng3r 2N^PS!#$#'}
+        actual = make_character('$@$G1Ng3r 2N^PS!#$#')
+        self.assertEqual(expected, actual)
 
 
