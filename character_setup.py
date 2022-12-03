3fdcde435b645ed9f9fd159c30ab596631001e79
cdoig4 <cdoig4@my.bcit.ca>
2022-12-02 18:10:50 -0800

    Add message informing user of Nerve level
    Add message informing user of Nerve level returning to normal after a fight()

--- character_setup.py
+++ character_setup.py
@@ -111,7 +111,8 @@ def you_win(character, enemy_name, challenge_name):
     """
     """
     if challenge_name == 'fight':
-        print(f"{enemy_name} slinks away, clearly feeling the shade of it all.\n")
+        print(f"{enemy_name} slinks away, clearly feeling the shade of it all.\n"
+              f"You regain composure after all the reads. You return to full Nerve.")
         increase = random.randint(8, 12)
         print('You win!')
         return power_up_or_down(character, [0, 0, 0, increase], True)
