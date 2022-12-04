d7423b5b2ab50c499bc3a95cc9adc3d53a590434
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-03 23:31:21 -0800

    Correct place board in character input

--- test_place_character_in_board.py
+++ test_place_character_in_board.py
@@ -10,7 +10,21 @@ from boards import place_character_in_board
 
 
 class TestPlaceCharacterInBoard(TestCase):
-    def test_place_character_in_board(self, _):
-        expected = [0]
-        actual = []
-        self.assertEqual(expected, actual)
+    def test_place_character_in_board(self):
+        board = '     #   #   #   #   #   #\n' \
+                   '    -------------------\n' \
+                   '   | !   !   !   !   ! | x  $\n' \
+                   '   |                    ---\n' \
+                   '   | !   R   !   !   !  [E] $\n' \
+                   '   |                    ---\n' \
+                   '   | !   !   !   !   ! | x  $\n' \
+                   '    -------------------\n'
+        expected = '     #   #   #   #   #   #\n' \
+                   '    -------------------\n' \
+                   '   |                   |    $\n' \
+                   '   |                    ---\n' \
+                   '   |             &   x  [x] $\n' \
+                   '   |                    ---\n' \
+                   '   | x   x   x   x   x | x  $\n' \
+                   '    -------------------\n'
+        self.assertEqual(expected, place_character_in_board(board, (1, 3)))
