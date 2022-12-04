00a6e6636dedf8e7e693c7763ff602dd9c0c07ce
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-04 00:14:51 -0800

    Change return_value to side_effect

--- test_get_directional_input_from_user.py
+++ test_get_directional_input_from_user.py
@@ -10,25 +10,25 @@ from controls import get_directional_input_from_user
 
 
 class TestGetDirectionalInputFromUser(TestCase):
-    @patch('builtins.input', return_value='W')
+    @patch('builtins.input', side_effect='W')
     def test_get_directional_input_from_user_W(self, _):
         game_input = [('W', 'Up'), ('S', 'Down'), ('A', 'Left'), ('D', 'Right'), ('0', 'Stats')]
         actual = get_directional_input_from_user(game_input, {})
         self.assertEqual('up', actual)
 
-    @patch('builtins.input', return_value='A')
+    @patch('builtins.input', side_effect='A')
     def test_get_directional_input_from_user_A(self, _):
         game_input = [('W', 'Up'), ('S', 'Down'), ('A', 'Left'), ('D', 'Right'), ('0', 'Stats')]
         actual = get_directional_input_from_user(game_input, {})
         self.assertEqual('left', actual)
 
-    @patch('builtins.input', return_value='S')
+    @patch('builtins.input', side_effect='S')
     def test_get_directional_input_from_user_S(self, _):
         game_input = [('W', 'Up'), ('S', 'Down'), ('A', 'Left'), ('D', 'Right'), ('0', 'Stats')]
         actual = get_directional_input_from_user(game_input, {})
         self.assertEqual('down', actual)
 
-    @patch('builtins.input', return_value='D')
+    @patch('builtins.input', side_effect='D')
     def test_get_directional_input_from_user_D(self, _):
         game_input = [('W', 'Up'), ('S', 'Down'), ('A', 'Left'), ('D', 'Right'), ('0', 'Stats')]
         actual = get_directional_input_from_user(game_input, {})
