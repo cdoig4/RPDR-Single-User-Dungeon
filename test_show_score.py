f6621b0f444f7722abd8e4de5cf1b1fd57371094
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-03 20:53:21 -0800

    Adjust formatting to fit inside 100 characters

--- test_show_score.py
+++ test_show_score.py
@@ -10,13 +10,17 @@ from controls import show_score
 
 class TestShowScore(TestCase):
     def test_stats_all_negative_values(self):
-        self.assertEqual(None, show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': -400, 'Talent': -8}))
+        self.assertEqual(
+            None, show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': -400, 'Talent': -8}))
 
     def test_stats_all_zero_values(self):
-        self.assertEqual(None, show_score({'Charisma': 0, 'Uniqueness': 0, 'Nerve': 0, 'Talent': 0}))
+        self.assertEqual(None,
+                         show_score({'Charisma': 0, 'Uniqueness': 0, 'Nerve': 0, 'Talent': 0}))
 
     def test_stats_all_positive_values(self):
-        self.assertEqual(None, show_score({'Charisma': 5, 'Uniqueness': 25, 'Nerve': 400, 'Talent': 8}))
+        self.assertEqual(None,
+                         show_score({'Charisma': 5, 'Uniqueness': 25, 'Nerve': 400, 'Talent': 8}))
 
     def test_stats_mix_of_values(self):
-        self.assertEqual(None, show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': 500, 'Talent': 0}))
+        self.assertEqual(None,
+                         show_score({'Charisma': -5, 'Uniqueness': -25, 'Nerve': 500, 'Talent': 0}))
