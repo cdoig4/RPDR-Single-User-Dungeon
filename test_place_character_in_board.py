b2662bfd9ef35b4861e21337942dcd940b64ea24
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-03 23:41:54 -0800

    Test for middle, exit, entry

--- test_place_character_in_board.py
+++ test_place_character_in_board.py
@@ -10,7 +10,7 @@ from boards import place_character_in_board
 
 
 class TestPlaceCharacterInBoard(TestCase):
-    def test_place_character_in_board(self):
+    def test_place_character_in_board_middle(self):
         board = '     #   #   #   #   #   #\n' \
                    '    -------------------\n' \
                    '   | !   !   !   !   ! | x  $\n' \
@@ -28,3 +28,59 @@ class TestPlaceCharacterInBoard(TestCase):
                    '   | x   x   x   x   x | x  $\n' \
                    '    -------------------\n'
         self.assertEqual(expected, place_character_in_board(board, (1, 3)))
+
+    def test_place_character_in_board_exit(self):
+        board = '    #   #   #   #   #   #   #   #   #   #\n' \
+                   '    x   x   x   x   x |[E]| x   x   x   x   $\n' \
+                   '                   ---     ---\n' \
+                   '    x   x   x   x | !   !   ! | x   x   x   $\n' \
+                   '                  |           |\n' \
+                   '    x   x   x   x | !   !   ! | x   x   x   $\n' \
+                   '                  |           |\n' \
+                   '    x   x   x   x | !   !   ! | x   x   x   $\n' \
+                   '                  |           |\n' \
+                   '    x   x   x   x | !   !   ! | x   x   x   $\n' \
+                   '       -----------             -----------\n' \
+                   '    x | !   !   !   !   !   !   !   !   ! | $\n' \
+                   '   ---                                    |\n' \
+                   '   <e.  !   Q   !   !   !   !   !   X   ! | $\n' \
+                   '   ---                                    |\n' \
+                   '    x | !   !   !   !   !   !   !   !   ! | $\n' \
+                   '       -----------------------------------\n'
+        expected = '    #   #   #   #   #   #   #   #   #   #\n' \
+                   '                      |[ ]|                 $\n' \
+                   '                   ---     ---\n' \
+                   '                  |           |             $\n' \
+                   '                  |           |\n' \
+                   '                  |           |             $\n' \
+                   '                  |           |\n' \
+                   '                  |           |             $\n' \
+                   '                  |           |\n' \
+                   '                  |           |             $\n' \
+                   '       -----------             -----------\n' \
+                   '      |                                   | $\n' \
+                   '   ---                                    |\n' \
+                   '   <&.  x   x   x   x   x   x   x   x   x | $\n' \
+                   '   ---                                    |\n' \
+                   '    x | x   x   x   x   x   x   x   x   x | $\n' \
+                   '       -----------------------------------\n'
+        self.assertEqual(expected, place_character_in_board(board, (6, 0)))
+
+    def test_place_character_in_board_door_entry(self):
+        board = '     #   #   #   #   #   #\n' \
+                   '    -------------------\n' \
+                   '   | !   !   !   !   ! | x  $\n' \
+                   '   |                    ---\n' \
+                   '   | !   R   !   !   !  [E] $\n' \
+                   '   |                    ---\n' \
+                   '   | !   !   !   !   ! | x  $\n' \
+                   '    -------------------\n'
+        expected = '     #   #   #   #   #   #\n' \
+                   '    -------------------\n' \
+                   '   |                   |    $\n' \
+                   '   |                    ---\n' \
+                   '   |                    [&] $\n' \
+                   '   |                    ---\n' \
+                   '   | x   x   x   x   x | x  $\n' \
+                   '    -------------------\n'
+        self.assertEqual(expected, place_character_in_board(board, (1, 5)))
