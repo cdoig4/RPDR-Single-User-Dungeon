093c5800bd24b23686bf63b382c482d1ca8116e8
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2022-12-15 12:46:56 -0800

    Instantiate base character

--- character.py
+++ character.py
@@ -1,5 +1,25 @@
 class Character:
-    pass
+    """
+    A class representing the character.
+    """
+
+    id_counter = 0
+
+    def __init__(self, name, charisma, uniqueness, nerve, talent, location, coordinates):
+        if name == '':
+            raise ValueError("Character name cannot be empty.")
+        else:
+            self.name = name
+
+        self.charisma = 10
+        self.uniqueness = 10
+        self.nerve = 10
+        self.talent = 10
+        self.location = 'werk_room'
+        self.coordinates = (0, 4)
+
+        self.id = Character.id_counter
+        Character.id_counter += 1
 
 
 def main():
